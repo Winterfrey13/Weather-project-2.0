7a6900d76b3cdc04b898ed149ceb2b73aedc6999
Winterfrey13 <oumououangre@gmail.com>
2023-11-19 23:43:26 -0500

    search functionalities using the api

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input")
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#real-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function searchCity(city) {
+  let apiKey = "0028ba044ca06fc2b3teda9ee7f81ob0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Montreal");
